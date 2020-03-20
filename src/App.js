7b4230ffd3850cf8a04c68a2fb6fbb8b04a6f1a6
Louise Bedwell <Louise-Bedwell@softwareimaging.com>
2020-03-20 13:27:25 +0000

    added firebase user object creationg

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import HomePage from './pages/homepage/homepage.component';
 import Header from './components/header/header.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, CreateUserProfileDocument } from './firebase/firebase.utils';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import './App.css';
@@ -20,11 +20,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsucscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
+    this.unsucscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth) {
+        const userRef = await CreateUserProfileDocument(userAuth);
 
-      console.log(user);
-
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
+      }
+      else {
+        this.setState({ currentUser: userAuth })
+      }
     })
   }
 
